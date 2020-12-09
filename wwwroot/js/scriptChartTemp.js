90added453ebe77218c383ec19be6d068dc107d3
emilieDelsol <emiliedelsol1@gmail.com>
2020-12-09 14:14:29 +0100

    [+] #2: add new controller beon, script carte, script chart temp, view indek beon et beon.css

---
+++ wwwroot/js/scriptChartTemp.js
@@ -0,0 +1,39 @@
+﻿var line = document.getElementById('tempsChart');
+var myLineChart = new Chart(line, {
+    type: 'line',
+    data: {
+        labels: ['date1', 'date2', 'date3', 'date4', 'date5', 'date6', 'date7', 'date8', 'date9', 'date10', 'date11', 'date12'],
+        datasets: [
+            {
+                label: "# temp max",
+                data: [11, 12, 15, 20, 25, 20, 10, 10, 12, 15, 20, 25],
+                radius: 0,
+                borderColor: "red"
+            },
+            {
+                label: "# temp min",
+                data: [1, 2, 5, 10, 15, 10, 1, 1, 2, 5, 10, 15],
+                radius: 0,
+                borderColor: "blue"
+            },
+            {
+                label: "# temp mean",
+                data: [6, 7, 10, 15, 20, 15, 6, 6, 7, 10, 15, 20],
+                radius: 0,
+                borderColor: "green"
+            }
+        ]
+    },
+    options: {
+        label: "diagramme",
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }
+            ]
+        }
+    }
+});
