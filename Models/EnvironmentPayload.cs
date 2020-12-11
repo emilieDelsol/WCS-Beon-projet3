6d2aff9bb887c69684a08b4ad6aee021eb83b0b8
IzyJu <julienizd11@gmail.com>
2020-12-11 11:10:42 +0100

    [+] #1: Connexion base de donnée

---
+++ Models/EnvironmentPayload.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeOn.Models
+{
+    [Table("environment_payload")]
+    public class EnvironmentPayload
+    {
+        /*[Column("id_device")]
+        public string device_Id { get; set; }*/
+
+        [Column("timestampevent")]
+        public DateTime dateEvent { get; set; }
+
+        [Column("seq_number")]
+        public int Id { get; set; }
+
+        [Column("computed_latitude")]
+        public double latitude { get; set; }
+
+        [Column("computed_longitude")]
+        public double longitude { get; set; }
+
+        [Column("radius")]
+        public int radius { get; set; }
+       
+        [Column("source")]
+        public int source { get; set; }
+        
+        [Column("status")]
+        public int status { get; set; }
+
+        [Column("frametype")]
+        public string frametype { get; set; }
+
+        [Column("batterylvl")]
+        public double batterylvl {get; set; }
+
+        [Column("timestamppayload")]
+        public DateTime datePayload { get; set; }
+
+        [Column("event_type")]
+        public string eventType { get; set; }
+
+        [Column("tmax")]
+        public int tmax { get; set; }
+
+        [Column("tmin")]
+        public int tmin { get; set; }
+
+        [Column("tmean")]
+        public int tmean { get; set; }
+
+        [Column("total_alert_duration")]
+        public int alert_duration { get; set; }
+      
+        [Column("smax")]
+        public double smax { get; set; }
+
+        [Column("total_shock")]
+        public double totalshock { get; set; }
+
+        [Column("cptshockovlastperiod")]
+        public int cptshockovlastperiod { get; set; }
+    }
+}
