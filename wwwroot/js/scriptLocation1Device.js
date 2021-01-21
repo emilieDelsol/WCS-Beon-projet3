adf4fc032761d29f5dca2e69f2e325ed640a8e8f
IzyJu <julienizd11@gmail.com>
2021-01-21 09:08:01 +0100

    [+] #10: add filtre

--- wwwroot/js/scriptLocation1Device.js
+++ wwwroot/js/scriptLocation1Device.js
@@ -3,6 +3,8 @@ let lat = 43.6043;
 let lon = 1.4437;
 let overview = null;
 let markerClusters;
+let lastlat = 0;
+let lastlon = 0
 
 function initMap() {
     let markers = [];
@@ -14,14 +16,23 @@ function initMap() {
         maxZoom: 20
     }).addTo(overview);
     for (env in jsDeviceEnvironment) {
-        let marker = L.marker([jsDeviceEnvironment[env].lat, jsDeviceEnvironment[env].lon]);
-        marker.bindPopup('<h5>device id: ' + env + '</h5> <p> Last contact: ' + jsDeviceEnvironment[env].lastContact + ' </p><p>Temp mean: ' + jsDeviceEnvironment[env].tMean + ' °C</p><p>Total shock: ' + jsDeviceEnvironment[env].totalShock + ' </p><p> Smax: ' + jsDeviceEnvironment[env].sMax + ' </p><p> BatteryLvl: ' + jsDeviceEnvironment[env].batteryLvl + '% </p><p><a href="/Beon/Dashboard?IdDevice=' + env + '">link to dashboard </a></p>');
-        markerClusters.addLayer(marker);
-        markers.push(marker);
+        let difflat = jsDeviceEnvironment[env].lat - lastlat;
+        let difflon = jsDeviceEnvironment[env].lon - lastlon;
+        if (difflat > 0.001 || difflon < -0.001) {
+            lastlat = jsDeviceEnvironment[env].lat;
+            lastlon = jsDeviceEnvironment[env].lon;
+            let marker = L.marker([jsDeviceEnvironment[env].lat, jsDeviceEnvironment[env].lon]).addTo(overview);
+            marker.bindPopup('<h5>device id: ' + env + '</h5> <p> Last contact: ' + jsDeviceEnvironment[env].lastContact + ' </p><p>Temp mean: ' + jsDeviceEnvironment[env].tMean + ' °C</p><p>Total shock: ' + jsDeviceEnvironment[env].totalShock + ' </p><p> Smax: ' + jsDeviceEnvironment[env].sMax + ' </p><p> BatteryLvl: ' + jsDeviceEnvironment[env].batteryLvl + '% </p><p><a href="/Beon/Dashboard?IdDevice=' + env + '">link to dashboard </a></p>');
+            markerClusters.addLayer(marker);
+            markers.push(marker);
+        }
+        else {
+            continue;
+        }
     }
-    let group = new L.featureGroup(markers);
-    overview.fitBounds(group.getBounds().pad(0.2));
-    overview.addLayer(markerClusters);
+    //let group = new L.featureGroup(markers);
+    //overview.fitBounds(group.getBounds().pad(0.2));
+    //overview.addLayer(markerClusters);
 }
 window.onload = function () {
     initMap();
