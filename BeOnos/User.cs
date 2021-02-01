f9d985c9a776242e05429ebfd08c03f27935d336
emilieDelsol <emiliedelsol1@gmail.com>
2021-02-01 14:49:21 +0100

    [+] #29 add beonos et beonosTest

---
+++ BeOnos/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BeOnos
+{
+
+        public class User
+        {
+            public String Password { private get; set; }
+            public String PasswordHash { get => BCrypt.Net.BCrypt.HashPassword(Password); }
+            public String Identifier { get; set; }
+
+            public Boolean CheckPassword(String password)
+            {
+                Boolean passwordsMatch = BCrypt.Net.BCrypt.Verify(password, PasswordHash);
+                if (passwordsMatch)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    
+}
