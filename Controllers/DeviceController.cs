dcfb984f740fa5217d871ebbf96408888fa11598
Corentin PREVOST <prevost.corentin@yahoo.com>
2021-02-04 10:43:41 +0100

    [+] Add vue.js + renaming views

---
+++ Controllers/DeviceController.cs
@@ -0,0 +1,25 @@
+﻿using BeOn.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace BeOn.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeviceController : ControllerBase
+    {
+        private readonly DeviceRepository _deviceRepository;
+
+        public DeviceController(DeviceRepository deviceRepository)
+        {
+            _deviceRepository = deviceRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllDevices()
+        {
+            return Ok(_deviceRepository.All);
+        }
+    }
+}
+
